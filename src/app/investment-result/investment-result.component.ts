9952aabea6b4e5363f51481a97ae2344e4cad49a
preet <preet@PREETHS_PC>
2024-09-10 13:55:20 -0400

    added Modules

--- src/app/investment-result/investment-result.component.ts
+++ src/app/investment-result/investment-result.component.ts
@@ -1,11 +1,8 @@
-import { CurrencyPipe } from '@angular/common';
-import { Component, computed, inject, input} from '@angular/core';
+import { Component, computed, inject} from '@angular/core';
 import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-result',
-  standalone: true,
-  imports: [CurrencyPipe],
   templateUrl: './investment-result.component.html',
   styleUrl: './investment-result.component.css'
 })
